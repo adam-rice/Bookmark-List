3837e16466f445502cb25884a5650013e6c56381
Adam Rice <adamcoltonrice@gmail.com>
2016-10-19 13:00:58 -0600

    bkm title url overflow addressed

--- scripts.js
+++ scripts.js
@@ -21,6 +21,27 @@ $create.on('click', function() {
   $title.focus();
 });
 
+$url.bind('keydown', function(e) {
+	if (e.keyCode==13) {
+    validateInputs();
+    clearFields();
+    counter();
+    $title.focus();
+	}
+});
+
+$('.list').on('click', '.unread-btn', function () {
+  $(this).parent().toggleClass('read');
+  $(this).toggleClass('read-btn-update');
+  $(this).siblings().toggleClass('delete-update txt-dec-update');
+  counter();
+});
+
+$('.list').on('click', '.delete', function() {
+  $(this).parent().remove();
+  counter();
+});
+
 function clearFields() {
   $title.val('');
   $url.val('');
@@ -38,11 +59,11 @@ function counter() {
  $unread.text(unread - read);
 }
 
-function addLink() {
+function addBkm() {
   $('ul').prepend(`
     <li class="new-bkm unread">
       <h2>${$title.val()}</h2><hr>
-      <a href="${$url.val()}">${$url.val()}</a><hr>
+      <a href="${$url.val()}" target="_blank">${$url.val()}</a><hr id="hr-lower">
       <button class="unread-btn" type="button" name="button">Read</button>
       <button class="delete" type="button" name="button">Delete</button>
     </li>
@@ -53,18 +74,6 @@ function validateInputs() {
   if ($title.val() === '' || $url.val() === '') {
    alert("Please enter you bookmark information!");
  } else {
-   addLink();
+   addBkm();
   }
 }
-
-$('.list').on('click', '.unread-btn', function () {
-  $(this).parent().toggleClass('read');
-  $(this).toggleClass('read-btn-update');
-  $(this).siblings().toggleClass('delete-update txt-dec-update');
-  counter();
-});
-
-$('.list').on('click', '.delete', function() {
-  $(this).parent().remove();
-  counter();
-});
