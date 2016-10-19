8c31d497c4b6904059cc101061e6b5b45b815fa0
Adam Rice <adamcoltonrice@gmail.com>
2016-10-19 14:50:01 -0600

    functionality for create btn now disabled if an input fi is empty

--- scripts.js
+++ scripts.js
@@ -1,46 +1,47 @@
 var $create = $('#create-btn');
 var $title = $('#title');
 var $url = $('#url');
-var $list = $('#list');
-
-
-// enableBtn();
-//
-// function enableBtn() {
-//   if ($title.val() === "" && $url.val() === "") {
-//     $create.attr('disabled', true); }
-//   else if ($title.val() !== "" && $url.val() !== "") {
-//     $create.removeAttr('disabled');
-//   }
-// }
+var $list = $('.list');
 
 $create.on('click', function() {
-  validateInputs();
-  clearFields();
-  counter();
-  $title.focus();
+  submitBkm();
 });
 
 $url.bind('keydown', function(e) {
-	if (e.keyCode==13) {
-    validateInputs();
-    clearFields();
-    counter();
-    $title.focus();
-	}
+  if (e.keyCode==13) {
+    submitBkm();
+  }
 });
 
-$('.list').on('click', '.unread-btn', function () {
+$title.keyup( function() {
+  enableCreate();
+});
+
+$url.keyup( function ()  {
+  enableCreate();
+});
+
+$list.on('click', '.unread-btn', function () {
   $(this).parent().toggleClass('read');
   $(this).toggleClass('read-btn-update');
   $(this).siblings().toggleClass('delete-update txt-dec-update');
   counter();
 });
 
-$('.list').on('click', '.delete', function() {
+$list.on('click', '.delete', function() {
   $(this).parent().remove();
   counter();
 });
+
+function enableCreate() {
+  var title = $title.val();
+  var url = $url.val();
+  if (title === "" || url === "") {
+    $create.attr('disabled', true); }
+  else if (title !== "" && url !== "") {
+    $create.attr('disabled', false);
+  }
+}
 
 function clearFields() {
   $title.val('');
@@ -48,32 +49,40 @@ function clearFields() {
 }
 
 function counter() {
- var $saved = $('#saved-bkm');
- var $read = $('#read-bkm');
- var $unread = $('#unread-bkm');
- var unread = $('.unread').length;
- var read = $('.read').length;
- var total = $('li').length;
- $saved.text(total);
- $read.text(read);
- $unread.text(unread - read);
-}
-
-function addBkm() {
-  $('ul').prepend(`
-    <li class="new-bkm unread">
-      <h2>${$title.val()}</h2><hr>
-      <a href="${$url.val()}" target="_blank">${$url.val()}</a><hr id="hr-lower">
-      <button class="unread-btn" type="button" name="button">Read</button>
-      <button class="delete" type="button" name="button">Delete</button>
-    </li>
-    `)
+  var $unread = $('#unread-bkm');
+  var $read = $('#read-bkm');
+  var $saved = $('#saved-bkm');
+  var unread = $('.unread').length;
+  var read = $('.read').length;
+  var total = $('li').length;
+  $unread.text(unread - read);
+  $read.text(read);
+  $saved.text(total);
 }
 
 function validateInputs() {
   if ($title.val() === '' || $url.val() === '') {
    alert("Please enter you bookmark information!");
- } else {
-   addBkm();
+  } else {
+      addBkm();
   }
 }
+
+function addBkm() {
+  $('ul').prepend(`
+    <li class="new-bkm unread">
+    <h2>${$title.val()}</h2><hr>
+    <a href="${$url.val()}" target="_blank">${$url.val()}</a><hr id="hr-lower">
+    <button class="unread-btn" type="button" name="button">Read</button>
+    <button class="delete" type="button" name="button">Delete</button>
+    </li>
+    `)
+  }
+
+  function submitBkm() {
+    validateInputs();
+    clearFields();
+    counter();
+    $title.focus();
+    $create.attr('disabled', true);
+  }
