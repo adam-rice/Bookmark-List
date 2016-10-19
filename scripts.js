07f4c08b2947d96fdbc6c02a9200af5f635df40c
Adam Rice <adamcoltonrice@gmail.com>
2016-10-18 23:47:31 -0600

    several var added to js and listener added to create

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
+var $create = $('#create-btn');
+var $title = $('#title');
+var $url = $('#url');
+
 $('.unread').on('click', '.unread-btn' ,function () {
   $(this).parent().toggleClass('read');
   $(this).toggleClass('read-btn-update');
   $(this).siblings().toggleClass('delete-update txt-dec-update');
+});
+
+// enableBtn();
+//
+// function enableBtn() {
+//   if ($title.val() === "" && $url.val() === "") {
+//     $create.attr('disabled', true); }
+//   else if ($title.val() !== "" && $url.val() !== "") {
+//     $create.removeAttr('disabled');
+//   }
+// }
+
+$create.on('click', function() {
+  alert('booooooom');
 });
